6603b511f482f8c66b8589f808230ff361da3e7a
Kota Aoyama <hongcha811@gmail.com>
2022-02-22 23:24:24 +0900

    updated module-info.java

--- udasecurity/image/module-info.java
+++ udasecurity/image/module-info.java
@@ -1,3 +1,9 @@
 module com.udacity.udasecurity.image {
-    exports com.udacity.udasecurity.security.service;
+    requires org.slf4j;
+    requires java.desktop;
+    requires software.amazon.awssdk.auth;
+    requires software.amazon.awssdk.core;
+    requires software.amazon.awssdk.regions;
+    requires software.amazon.awssdk.services.rekognition;
+    exports com.udacity.udasecurity.image.service;
 }
--- udasecurity/security/module-info.java
+++ udasecurity/security/module-info.java
@@ -1,3 +1,8 @@
 module com.udacity.udasecurity.security {
     requires com.udacity.udasecurity.image;
+    requires miglayout;
+    requires java.desktop;
+    requires java.prefs;
+    requires com.google.gson;
+    requires java.sql;
 }
