2890c6401f17af6b39e1a875930d21fb669a53eb
Kota Aoyama <hongcha811@gmail.com>
2022-02-24 07:59:45 +0900

    removed unnecessary lines

--- udasecurity/security/src/test/java/com/udacity/udasecurity/security/service/SecurityServiceTest.java
+++ udasecurity/security/src/test/java/com/udacity/udasecurity/security/service/SecurityServiceTest.java
@@ -32,12 +32,8 @@ public class SecurityServiceTest {
     @ParameterizedTest
     @MethodSource("differentSensorTypeAndSensorActive")
     public void changeSensorActivated_whenAlertArmed_pendingAlarm(Sensor sensor, boolean active) {
-        Mockito.when(securityRepository.getArmingStatus())
-                .thenReturn(ArmingStatus.ARMED_HOME);
-        Mockito.when(securityRepository.getAlarmStatus())
-                .thenReturn(AlarmStatus.NO_ALARM);
-
-        securityService.addSensor(sensor);
+        Mockito.when(securityRepository.getArmingStatus()).thenReturn(ArmingStatus.ARMED_HOME);
+        Mockito.when(securityRepository.getAlarmStatus()).thenReturn(AlarmStatus.NO_ALARM);
         securityService.changeSensorActivationStatus(sensor, active);
 
         Mockito.verify(securityRepository).setAlarmStatus(AlarmStatus.PENDING_ALARM);
@@ -46,15 +42,16 @@ public class SecurityServiceTest {
     @ParameterizedTest
     @MethodSource("differentSensorTypeAndSensorActive")
     public void changeSensorActivated_whenAlertArmedAndPendingAlarmStatus_alarm(Sensor sensor, boolean active) {
-        Mockito.when(securityRepository.getArmingStatus())
-                .thenReturn(ArmingStatus.ARMED_AWAY);
-        Mockito.when(securityRepository.getAlarmStatus())
-                .thenReturn(AlarmStatus.PENDING_ALARM);
-
-        securityService.addSensor(sensor);
+        Mockito.when(securityRepository.getArmingStatus()).thenReturn(ArmingStatus.ARMED_AWAY);
+        Mockito.when(securityRepository.getAlarmStatus()).thenReturn(AlarmStatus.PENDING_ALARM);
         securityService.changeSensorActivationStatus(sensor, active);
 
         Mockito.verify(securityRepository).setAlarmStatus(AlarmStatus.ALARM);
+    }
+
+    @Test
+    public void getAlarmStatus_whenPendingAlarmStatusAndAllSensorsInactive_noAlarm() {
+
     }
 
     private static Stream<Arguments> differentSensorTypeAndSensorActive() {
