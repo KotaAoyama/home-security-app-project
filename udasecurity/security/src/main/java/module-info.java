7326d2d481643c2b283cc033124fd65b05df2b3a
Kota Aoyama <hongcha811@gmail.com>
2022-03-11 22:42:34 +0900

    build successfully

--- udasecurity/security/src/main/java/module-info.java
+++ udasecurity/security/src/main/java/module-info.java
@@ -1,8 +1,9 @@
 module com.udacity.udasecurity.security {
     requires com.udacity.udasecurity.image;
-    requires miglayout;
     requires java.desktop;
     requires java.prefs;
     requires com.google.gson;
     requires java.sql;
+    requires guava;
+    requires miglayout.swing;
 }
